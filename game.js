72ba230d05ab21f692db453507dcb1dcabdaf75f
howard <howardthomas13@gmail.com>
2022-08-13 01:38:26 -0400

    Initial Commit
    Simple Rock Paper Scissors game made with javascript

---
+++ game.js
@@ -0,0 +1,65 @@
+const computer_hand = ['rock','paper','scissors']
+let wins = 0
+let lose = 0
+let tie = 0
+
+function playerSelection(params) {
+    return String(prompt('What is your move: Rock, Paper, Scissor'))
+}
+
+function random_hand(min = 0, max = 2) {
+    return Math.floor(Math.random()* (max - min + 1)) + min;
+}
+
+function getComputerChoice(params) {
+    random_number = random_hand()
+    return computer_hand[random_number]
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'rock'){
+        if(computerSelection === 'rock'){
+            tie ++
+            return 'Tie Game'
+        } else if (computerSelection === 'paper'){
+            lose ++
+            return 'Computer Wins'
+        } else if (computerSelection === 'scissors'){
+            wins ++
+            return 'Player Wins'
+        }
+    }else if (playerSelection === 'paper'){
+        if(computerSelection === 'rock'){
+            wins ++
+            return 'Player Wins'
+        } else if (computerSelection === 'paper'){
+            tie ++
+            return 'Tie Game'
+        } else if (computerSelection === 'scissors'){
+            lose ++
+            return 'Computer Wins'
+        }
+    } else if (playerSelection === 'scissors'){
+        if(computerSelection === 'rock'){
+            lose ++
+            return 'Computer Wins'
+        } else if (computerSelection === 'paper'){
+            wins ++
+            return 'Player Wins'
+        } else if (computerSelection === 'scissors'){
+            tie++
+            return 'Tie Game'
+        }
+    }
+}
+
+function game(params) {
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound(playerSelection(),getComputerChoice()))
+        console.log('Wins: ' + wins)
+        console.log('Loses: ' + lose)
+        console.log('Tie: ' + tie)
+    }
+}
+
+game()
